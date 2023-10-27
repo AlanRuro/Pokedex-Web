ecb2f2c4f857f3af9c09a7e065359853f5514dbe
Alan Ruelas <A01641426@tec.mx>
2023-10-27 01:25:19 -0600

    Verification for moves

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -3,8 +3,10 @@ require 'pokemon/pokemon_client'
 class MovesController < ApplicationController
     def index
         @pokemon = Pokemon.find_by(name: params[:pokemon_id])
-        pokemon_moves = ::PokemonSupport::PokemonClient.moves_by_pokemon(params[:pokemon_id])
-        pokemon_moves.each { |move| @pokemon.moves.create(move) }
+        if @pokemon.moves.empty?
+            pokemon_moves = ::PokemonSupport::PokemonClient.moves_by_pokemon(params[:pokemon_id])
+            pokemon_moves.each { |move| @pokemon.moves.create(move) }
+        end
         @moves = @pokemon.moves
     end
 end
